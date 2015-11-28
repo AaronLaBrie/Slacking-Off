4ed9121e6c018ca64203b44546db1c96d9ef68f5
AaronLaBrie <aaron@aaronlabrie.com>
2015-11-27 23:58:54 -0600

    Fixing port

--- main.js
+++ main.js
@@ -5,12 +5,18 @@ var flipper = require('./controllers/flipper');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 3000));
+
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(request, response) {
+  response.send("It's alive!");
+});
 
 app.post('/digitizer', digitizer.digitizer);
 app.post('/flipper', flipper.flipper);
 
-var server = app.listen(3000, function () {
+var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
   var port = server.address().port;
 });
