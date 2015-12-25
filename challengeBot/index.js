ff8ef33f88d0d69a275c41db2bbc7702bad791a3
AaronLaBrie <aaron@aaronlabrie.com>
2015-12-25 16:11:49 -0600

    Better response

--- challengeBot/index.js
+++ challengeBot/index.js
@@ -22,7 +22,11 @@ module.exports = (function() {
     if(params[1].slice(0,2) != '<@') {
       bot.reply(message, "I don't understand :disappointed:");
     } else {
+      //shift off the word 'challenge', then get the target
+      params.shift();
       var target = params.shift();
+
+      //the rest should be the challenge.
       var challenge = params.join(" ");
       bot.reply(message, target + ", you have been challenged: " + challenge);
     }
