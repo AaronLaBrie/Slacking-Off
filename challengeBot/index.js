f00c00ac3524004c2a687fd1493d3268c6fde6f7
AaronLaBrie <aaron@aaronlabrie.com>
2015-12-25 15:59:42 -0600

    Let's try some logic.

--- challengeBot/index.js
+++ challengeBot/index.js
@@ -10,14 +10,21 @@ module.exports = (function() {
 
   controller.spawn({
     token: process.env.CHALLENGEBOT_TOKEN,
-  }).startRTM()
+  }).startRTM();
 
-  controller.hears('hello','direct_message,direct_mention,mention',function(bot,message) {
-    bot.reply(message,'Hello yourself.');
+  controller.hears('ping', 'direct_message,direct_mention,mention', function(bot, message) {
+    bot.reply(message, 'PONG');
   });
 
-  controller.hears('challenge','direct_mention,mention',function(bot,message) {
-    bot.reply(message,'A challenge!: ' + message.text);
+  controller.hears('challenge', 'direct_mention,mention', function(bot, message) {
+    var params = message.text.split(" ");
+    if(params[0].charAt != '@') {
+      bot.reply(message, "I don't understand :dissapointed:");
+    } else {
+      var target = params.shift();
+      var challenge = params.join(" ");
+      bot.reply(message, target + ", you have been challenged: " + challenge);
+    }
   });
 
   return app;
