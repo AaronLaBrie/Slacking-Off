c63334b8305c39dded921b1d6f5d75b412009808
AaronLaBrie <aaron@aaronlabrie.com>
2015-12-26 10:51:34 -0600

    Let's get us some data

--- challengeBot/index.js
+++ challengeBot/index.js
@@ -1,8 +1,11 @@
 var Botkit = require('botkit');
 var express = require('express');
+var monk = require('monk');
 
 module.exports = (function() {
   var app = express.Router();
+  var errorText = "Something broke :cry:";
+  var db = monk(process.env.MONGOLAB_URI);
 
   var controller = Botkit.slackbot({
     debug: false
@@ -12,13 +15,30 @@ module.exports = (function() {
     token: process.env.CHALLENGEBOT_TOKEN,
   }).startRTM();
 
+  //Ping
   controller.hears('ping', 'direct_message,direct_mention,mention', function(bot, message) {
     bot.reply(message, 'PONG');
   });
 
+  //Add User
+  controller.hears('add me', 'direct_mention,mention', function(bot, message) {
+    //Make sure they aren't already in the game
+    db.get('users').findById(req.params.id, function (err, doc) {
+      if(err) { return bot.reply(message, errorText); }
+      if(doc) { return bot.reply(message, "You're already playing :face_with_rolling_eyes:"); }
+      //Add them.
+      req.db.get('users').insert({}, function (err, doc) {
+        if(err) {  return bot.reply(message, errorText); }
+        bot.reply(mesage, "Added! :tada:");
+      });
+    });
+  });
+
+  //Issue Challenge
   controller.hears('challenge', 'direct_mention,mention', function(bot, message) {
     var params = message.text.split(" ");
-    console.log(params)
+
+    //Check for a target
     if(params[1].slice(0,2) != '<@') {
       bot.reply(message, "I don't understand :disappointed:");
     } else {
@@ -30,6 +50,14 @@ module.exports = (function() {
       var challenge = params.join(" ");
       bot.reply(message, target + ", you have been challenged: " + challenge);
     }
+  });
+
+  //User test
+  controller.hears('challenge', 'direct_mention,mention', function(bot, message) {
+    db.get('users').find({}, function (err, docs){
+      if(err) { res.status(500).send(err) }
+      bot.reply(message, docs);
+    });
   });
 
   return app;
