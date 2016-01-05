3cff03872d32a26457a187b9f1999aa366080a71
AaronLaBrie <aaron@aaronlabrie.com>
2016-01-04 19:37:17 -0600

    Checking payload

--- challengeBot/userHandlers.js
+++ challengeBot/userHandlers.js
@@ -4,8 +4,7 @@ var userHandlers = {
   addUser: function(bot, message) {
     bot.api.users.info({user: message.user}, function(err, response) {
       if(err) { return bot.reply(message, errorText); }
-      console.log(response);
-      bot.reply(message, "hold up");
+      bot.reply(message, response.user.name);
     });
   }
 }
