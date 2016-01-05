19c769144b4fe4f564043b4a5f5179fb7258076d
AaronLaBrie <aaron@aaronlabrie.com>
2016-01-04 22:27:47 -0600

    Add things to a database?

--- challengeBot/userHandlers.js
+++ challengeBot/userHandlers.js
@@ -1,10 +1,26 @@
+var monk = require('monk');
+var db = monk(process.env.MONGOLAB_URI);
 var errorText = "Something broke :cry:";
 
 var userHandlers = {
+
+  //Add a new user to the game! Make sure they're not already playing first.
   addUser: function(bot, message) {
     bot.api.users.info({user: message.user}, function(err, data) {
       if(err) { return bot.reply(message, errorText); }
-      bot.reply(message, JSON.stringify(data));
+      db.get('users').findById(message.user, function (err, doc){
+        if(doc) {
+          return bot.reply(message, "You're already playing :face_with_rolling_eyes:");
+        } else {
+          //rename id so mongo isn't dumb
+          data.user["id"] = data.user["_id"];
+          delete data.user["id"];
+          db.get('users').insert(user, function (err, doc) {
+            if(err) { return bot.reply(message, errorText); }
+            return bot.reply(message, "Alright dude, you're in the game!");
+          });
+        }
+      });
     });
   }
 }
