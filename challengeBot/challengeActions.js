5d065019622182533d8086841abf71c2eff85f84
AaronLaBrie <aaron@aaronlabrie.com>
2016-01-05 23:38:25 -0600

    Didn't read syntax.

--- challengeBot/challengeActions.js
+++ challengeBot/challengeActions.js
@@ -29,7 +29,7 @@ var challengeActions = {
           //Now we make sure that the target doesn't already have a challenge from the sender.
           db.get('challenges').find({to: targetId, from: message.user, completed: false}, function(err, challenge) {
             if(err) { return bot.reply(message, errorText); }
-            if(challenge.isArray() && challenge.length > 0) {
+            if(Array.isArray(challenge) && challenge.length > 0) {
               return bot.reply(message, "You already have a challenge to <@" + targetId + ">: " + challenge.text);
             }
 
