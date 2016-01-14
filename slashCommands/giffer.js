205e52b540563d93319b561a84c21e1c983fbf2c
AaronLaBrie <aaron@aaronlabrie.com>
2016-01-14 10:12:24 -0600

    Giff

---
+++ slashCommands/giffer.js
@@ -0,0 +1,25 @@
+var giffer = function(req, res) {
+
+  if (req.body.token != process.env.GIFFER_TOKEN) {
+    res.status(401).send("Not from our team, sorry brah.");
+    return;
+  }
+
+  var giffText = "";
+  var giff = "༼ つ ◕_◕ ༽つ giff ";
+
+  if(req.body.text) {
+    giffText = giff + "diretide";
+  } else {
+    giffText = giff + req.body.text;
+  }
+
+  var result = {
+    response_type: "in_channel",
+    text: giffText,
+  };
+
+  res.send(result);
+};
+
+module.exports = giffer;
--- slashCommands/index.js
+++ slashCommands/index.js
@@ -1,5 +1,6 @@
 var digitizer = require('./digitizer');
 var flipper = require('./flipper');
+var giffer = require('./giffer');
 var express = require('express');
 
 module.exports = (function() {
@@ -7,6 +8,7 @@ module.exports = (function() {
 
   app.post('/digitizer', digitizer);
   app.post('/flipper', flipper);
+  app.post('/giffer', giffer);
 
   return app;
 })();
