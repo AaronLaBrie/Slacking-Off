2b78e0596f1f82d919eb9695f291fd531c293786
AaronLaBrie <aaron@aaronlabrie.com>
2016-05-23 10:43:32 -0500

    Clapping y u no work

--- slashCommands/clap.js
+++ slashCommands/clap.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
 
-  const clapper = (clapText) => {
-    return clapText.trim().toUpperCase().split(' ').join(' :clap: ') + ' :clap:'
+  const clapper = (text) => {
+    return text.trim().toUpperCase().split(' ').join(' :clap: ') + ' :clap:'
   }
 
   const result = {
