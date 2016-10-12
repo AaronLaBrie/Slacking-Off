01cef3b3cb2cb2ca514ee5d1c49738d69c3cc4e8
Aaron LaBrie <aaron.labrie@returnpath.com>
2016-10-12 15:14:09 -0500

    Don't try to format emoji content

--- slashCommands/alert.js
+++ slashCommands/alert.js
@@ -1,9 +1,13 @@
 module.exports = (req, res) => {
-  const text = req.body.text ? req.body.text : 'alert'
+  const text = req.body.text ? req.body.text.trim() : 'alert'
+
+  if (text.charAt(0) !== ':'){
+    text = `*${text.toUpperCase()}* `
+  }
 
   const result = {
     response_type: 'in_channel',
-    text: `:siren:  *${text.toUpperCase()}*  :siren:`
+    text: `:siren:  ${text}  :siren:`
   }
 
   res.send(result)
