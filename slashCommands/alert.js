76d9bc4ea342eabf83da97b7afe3718d8a8ec0f3
Aaron LaBrie <aaron@context.io>
2017-08-09 14:29:48 -0700

    Trim

--- slashCommands/alert.js
+++ slashCommands/alert.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   let text = req.body.text ? req.body.text.trim() : 'alert'
 
   if (text.charAt(0) !== ':'){
-    text = `*${text.toUpperCase()}* `
+    text = `*${text.toUpperCase().trim()}* `
   }
 
   const result = {
