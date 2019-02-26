40d0170067827a76ed60cf5e31df6d73c2bf16b9
Aaron LaBrie <aaron@aaronlabrie.com>
2019-02-26 01:21:46 -0600

    test

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // Base route for the slash commands
 app.use('/commands', slashCommands())
 
+// Pretend to use oauth
+app.get('/oauth', (req, res) => {
+  res.send('Success!')
+})
+
 // 404 if nothing else got hit.
 app.use((req, res) => res.status(404).send('404: Not Found'))
 
