9fc567a42dc50706abaed46cbfc9bd097e6f354e
Aaron LaBrie <aaron@aaronlabrie.com>
2019-02-26 01:23:32 -0600

    index

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // Base route for the slash commands
 app.use('/commands', slashCommands())
 
+app.get('/', (req, res) => {
+  res.send('<a href="https://github.com/AaronLaBrie/slashy">home</a>')
+})
+
 // Pretend to use oauth
 app.get('/oauth', (req, res) => {
   res.send('Success!')
