737f076c3e4099f8b2acbea2033448713e8bcf31
aaronlabrie <aaron@aaronlabrie.com>
2019-08-22 20:49:59 -0500

    naming

--- src/commands/ceb.ts
+++ src/commands/ceb.ts
@@ -4,7 +4,7 @@ import { RequestHandler } from 'express'
 export const ceb: RequestHandler = (_, res) => {
   const length = Math.floor(Math.random() * 100)
   const EEE = 'E'.repeat(length)
-  const text = `C${EEE}B :dota-rage:`
+  const text = `C${EEE}B :dota_rage:`
 
   const result = {
     response_type: 'in_channel',
